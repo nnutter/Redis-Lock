d96f1bd5a8ec59b9b63f5de45fb1c0f9b18eac2c
Nathan Nutter <iam@nnutter.com>
2012-03-22 16:47:55 -0500

    first working prototype

--- lib/Redis/Lock.pm
+++ lib/Redis/Lock.pm
@@ -4,45 +4,54 @@ use warnings;
 package Redis::Lock;
 
 sub new {
-    my $class = shift;
-    my %args  = @_;
-
-    my $redis = delete $args{redis};
-    return bless {
-        redis => $redis,
-    }, $class;
+	my $class = shift;
+	my %args  = @_;
+	my $redis = delete $args{redis} || die;
+	my $key   = delete $args{key} || die;
+	my $value = delete $args{value} || die;
+	return bless {
+		redis => $redis,
+		key   => $key,
+		value => $value,
+	}, $class;
 }
 
+sub redis { return shift->{redis} }
+sub key   { return shift->{key}   }
+sub value { return shift->{value} }
+
 sub lock {
-    my $self  = shift;
-    my $key   = shift;
-    my $value = shift;
+	my $self  = shift;
+	my %options          = @_;
+	my $refresh_interval = delete $options{refresh_interval};
+	my $timeout          = delete $options{timeout};
 
-    my %options          = @_;
-    my $keep_alive       = delete $options{keep_alive};
-    my $refresh_interval = delete $options{refresh_interval};
-    my $timeout          = delete $options{timeout};
+	unless ($self->redis->setnx($self->key => $self->value)) {
+		return;
+	}
+	if ($timeout) {
+		require heartbeat;
+		my ($ip, $port)    = split(':', $self->redis->{server});
+		my $key = $self->key;
+		$self->{heartbeat} = heartbeat::start_pacer($ip, $port, $key, $refresh_interval, $timeout);
+	}
 
-    $self->redis->setnx($key => $value);
-    if ($keep_alive) {
-        require heartbeat;
-        my ($ip, $port)    = split(':', $self->redis->{server});
-        $self->{heartbeat} = heartbeat::start_pacer($ip, $port, $key, $refresh_interval, $timeout);
-    }
-
-    return;
+	return 1;
 }
 
 sub release {
-    my $self = shift;
-    my $key  = shift;
+	my $self = shift;
 
-    $self->redis->del($key);
-    if ($self->{heartbeat}) {
-        heartbeat::stop_pacer($self->{heartbeat});
-    }
+	unless ($self->redis->del($self->key)) {
+		return;
+	}
+	if ($self->{heartbeat}) {
+		heartbeat::stop_pacer($self->{heartbeat});
+	}
 
-    return;
+	return 1;
 }
+
+sub DESTROY { shift->release }
 
 1;
